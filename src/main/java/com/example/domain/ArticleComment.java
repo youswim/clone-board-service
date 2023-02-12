0e1e123266375e3f3dbe311ef859cbb9c29ae084
youswim <youswim96@gmail.com>
2023-02-13 01:36:27 +0900

    feat(Articlecomment.java): 조건 추가 & 편의 코드 추가

--- src/main/java/com/example/domain/ArticleComment.java
+++ src/main/java/com/example/domain/ArticleComment.java
@@ -1,14 +1,74 @@
 package com.example.domain;
 
-import java.time.LocalDateTime;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ArticleComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
     private Article article;
+
+    @Setter
+    @Column(nullable = false, length = 500)
     private String content;
 
+
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
+
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
