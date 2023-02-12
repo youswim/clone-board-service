2e0c85b350f5441eb0a20f2b22e8c817379c3b08
youswim <youswim96@gmail.com>
2023-02-12 23:08:29 +0900

    feat(Article.java): 엔티티 조건 추가 & 편의 코드 추가
    1. NotNull, 필드 길이 제한 조건 추가
    2. 생성자, 팩토리 메서드, equals, hashcode 추가

--- src/main/java/com/example/domain/Article.java
+++ src/main/java/com/example/domain/Article.java
@@ -1,8 +1,6 @@
 package com.example.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -10,6 +8,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @ToString
@@ -20,6 +19,8 @@ import java.time.LocalDateTime;
         @Index(columnList = "createdBy")
 })
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// Hibernate 는 기본 생성자를 가져야 하고, private 가 아닌 접근제어자를 사용해야 한다.
 public class Article {
 
     @Id
@@ -27,23 +28,53 @@ public class Article {
     private Long id;
 
     @Setter
+    @Column(nullable = false)
     private String title;
 
     @Setter
+    @Column(nullable = false, length = 10000)
     private String content;
 
     @Setter
     private String hashtag;
 
     @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
 
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
 
     @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    } // 생성자를 private 로 한다
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    } // 팩토리 메서드
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        // java 14 pattern matching
+        return id != null && id.equals(article.id);
+        // 영속화 하지 않은 데이터는 동등성 검사에서 모두 탈락한다.
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
