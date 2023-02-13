1c08533ed17c3565783aaca9c9a362cd22528620
youswim <youswim96@gmail.com>
2023-02-13 14:05:20 +0900

    feat(JpaRepositoryTest): 테스트 코드 틀 추가

---
+++ src/test/java/com/example/repository/JpaRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.example.repository;
+
+
+import com.example.config.JpaConfig;
+import com.example.domain.Article;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Autowired
+    JpaRepositoryTest(ArticleRepository articleRepository, ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void selectTest() {
+
+        List<Article> articles = articleRepository.findAll();
+
+        Assertions.assertThat(articles).isNotNull().hasSize(0);
+    }
+}
