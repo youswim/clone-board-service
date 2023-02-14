a2a49b091d40fd9ac8dacb9bd1072eecfcbf5a8b
youswim <youswim96@gmail.com>
2023-02-14 12:18:14 +0900

    feat: Spring Data Rest 적용, HAL 적용

--- src/main/java/com/example/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.example.repository;
 
 import com.example.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.example.repository;
 
 import com.example.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
