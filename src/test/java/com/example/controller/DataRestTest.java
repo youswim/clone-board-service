dce9f173928c8b896b9053cb72783aa9f33fcd2d
youswim <youswim96@gmail.com>
2023-02-15 04:53:41 +0900

    test: 테스트 코드 제외
    해당 테스트 코드는 spring data api 테스트 코드이므로
    꼭 필요한 테스트가 아님.
    빠른 테스트 실행을 위해 제거

--- src/test/java/com/example/controller/DataRestTest.java
+++ src/test/java/com/example/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
+@Disabled("Spring Data Rest 통합 테스트는 불필요하므로 제외")
 @DisplayName("Data Rest - API 테스트")
 @Transactional
 @AutoConfigureMockMvc
