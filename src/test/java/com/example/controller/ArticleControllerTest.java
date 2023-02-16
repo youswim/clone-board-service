f9d23b57b80b443e4f012d7c6c284463084e7160
youswim <youswim96@gmail.com>
2023-02-16 16:43:47 +0900

    fix(ArticleControllerTest.java): contentType확인 로직 수정
    cotentType() => contentTypeCompatibleWith() 로 변경하여 정확히 일치하지 않더라도 호환된다면 통과하도록 수정
    중복되는 클래스 이름을 static필드로 이동

--- src/test/java/com/example/controller/ArticleControllerTest.java
+++ src/test/java/com/example/controller/ArticleControllerTest.java
@@ -7,10 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@Disabled // 테스트코드를 먼저 작성해둔 상태라서, 테스트가 실패한다. gradle build에 성공하기 위해서 어노테이션 붙임
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @DisplayName("View 컨트롤러 - 게시글")
 @WebMvcTest(ArticleController.class) // 원하는 컨트롤러 클래스만 빈으로 등록해서 테스트 가능. 가벼운 테스트를 위함
 class ArticleControllerTest {
@@ -24,44 +24,44 @@ class ArticleControllerTest {
     @Test
     public void giveNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
 
-        mvc.perform(MockMvcRequestBuilders.get("/articles"))
-                .andExpect(MockMvcResultMatchers.status().isOk()) // HTTP STATUS CODE가 OK인지
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML)) // CONTENT TYPE확인
-                .andExpect(MockMvcResultMatchers.view().name("articles/index")) // 반환되는 뷰의 이름 확인
-                .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));// View로 넘기는 Model에 "articles"라는 이름의 key가 있는지
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk()) // HTTP STATUS CODE가 OK인지
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)) // CONTENT TYPE확인
+                .andExpect(view().name("articles/index")) // 반환되는 뷰의 이름 확인
+                .andExpect(model().attributeExists("articles"));// View로 넘기는 Model에 "articles"라는 이름의 key가 있는지
     }
-
+    @Disabled
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void giveNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
 
-        mvc.perform(MockMvcRequestBuilders.get("/articles/1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
-                .andExpect(MockMvcResultMatchers.view().name("articles/detail"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("article")) // 게시글
-                .andExpect(MockMvcResultMatchers.model().attributeExists("articleComments")); // 댓글
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article")) // 게시글
+                .andExpect(model().attributeExists("articleComments")); // 댓글
     }
-
+    @Disabled
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
     public void giveNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
 
-        mvc.perform(MockMvcRequestBuilders.get("/articles/search"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))// Search Veiw는 Model에 넣을 Attribute가 없음
-                .andExpect(MockMvcResultMatchers.view().name("articles/search"));
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))// Search Veiw는 Model에 넣을 Attribute가 없음
+                .andExpect(view().name("articles/search"));
 
     }
-
+    @Disabled
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
     public void giveNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
 
-        mvc.perform(MockMvcRequestBuilders.get("/articles/search-hashtag"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))// Search hashtag Veiw는 Model에 넣을 Attribute가 없음
-                .andExpect(MockMvcResultMatchers.view().name("articles/search-hashtag"));
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))// Search hashtag Veiw는 Model에 넣을 Attribute가 없음
+                .andExpect(view().name("articles/search-hashtag"));
 
     }
 }
