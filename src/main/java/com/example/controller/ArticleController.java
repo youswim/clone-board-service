5e87a886e1e644bdb892b9204d03968d708c10f3
youswim <youswim96@gmail.com>
2023-03-02 22:47:59 +0900

    feat: ArticleController 기능 구현 & 테스트 코드 변경
    컨트롤러를 구현하고,
    컨트롤러의 기능에 맞게 테스트 코드를 수정함

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -1,27 +1,42 @@
 package com.example.controller;
 
+import com.example.domain.type.SearchType;
+import com.example.dto.response.ArticleResponse;
+import com.example.dto.response.ArticleWithCommentsResponse;
+import com.example.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
+    private final ArticleService articleService;
+
     @GetMapping
-    public String articles(ModelMap map) {
-        map.addAttribute("articles", List.of());
+    public String articles(
+            @RequestParam(required = false) SearchType searchType, // 파라미터가 없더라도 검색이 가능함 (전체 조회)
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map) {
+        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
-        map.addAttribute("article", "article"); // TODO: 실제 데이터를 찾아서 넣어줘야 하는 부분
-        map.addAttribute("articleComments", List.of());
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+        map.addAttribute("article", article); // TODO: 실제 데이터를 찾아서 넣어줘야 하는 부분
+        map.addAttribute("articleComments", article.articleCommentsResponse());
 
         return "articles/detail";
     }
--- src/test/java/com/example/controller/ArticleControllerTest.java
+++ src/test/java/com/example/controller/ArticleControllerTest.java
@@ -1,16 +1,27 @@
 package com.example.controller;
 
 import com.example.config.SecurityConfig;
+import com.example.dto.ArticleWithCommentsDto;
+import com.example.dto.UserAccountDto;
+import com.example.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
@@ -19,6 +30,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ArticleControllerTest {
     private final MockMvc mvc;
 
+    @MockBean
+    private ArticleService articleService;
+
     ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
@@ -26,23 +40,34 @@ class ArticleControllerTest {
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void giveNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        // given
+        given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
 
+        // when & then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk()) // HTTP STATUS CODE가 OK인지
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)) // CONTENT TYPE확인
                 .andExpect(view().name("articles/index")) // 반환되는 뷰의 이름 확인
                 .andExpect(model().attributeExists("articles"));// View로 넘기는 Model에 "articles"라는 이름의 key가 있는지
+
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
     }
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void giveNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        // given
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 
-        mvc.perform(get("/articles/1"))
+        // when & then
+        mvc.perform(get("/articles/" + articleId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article")) // 게시글
                 .andExpect(model().attributeExists("articleComments")); // 댓글
+
+        then(articleService).should().getArticle(articleId);
     }
     @Disabled
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
@@ -64,6 +89,34 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))// Search hashtag Veiw는 Model에 넣을 Attribute가 없음
                 .andExpect(view().name("articles/search-hashtag"));
+    }
 
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(1L,
+                "uno",
+                "pw",
+                "uno@mail.com",
+                "Uno",
+                "memo",
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
     }
 }
