d5192c49693f78ac287acffe77d4a3146842d0e0
youswim <youswim96@gmail.com>
2023-03-16 23:19:25 +0900

    [Refactor]: Serializable 제거
    JPA Buddy 를 이용해 작성하다보니 DTO에 Serializable 가 들어가버림
    우리 프로젝트는 직렬화로 Jackson을 사용하므로 Serializable이 필요하지 않음.
    따라서 해당 부분 삭제

--- src/main/java/com/example/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/example/dto/response/ArticleWithCommentsResponse.java
@@ -3,7 +3,6 @@ package com.example.dto.response;
 
 import com.example.dto.ArticleWithCommentsDto;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -18,7 +17,7 @@ public record ArticleWithCommentsResponse(
         String email,
         String nickname,
         Set<ArticleCommentResponse> articleCommentsResponse
-) implements Serializable {
+) {
 
     public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
         return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
