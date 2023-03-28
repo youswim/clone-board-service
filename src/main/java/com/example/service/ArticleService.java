e908bc00b81775bfbcc82b8e8983d313a95ab4d5
youswim <youswim96@gmail.com>
2023-03-28 20:27:40 +0900

    [Feat]: 변경사항에 따른 Service코드 뼈대 재구성

--- src/main/java/com/example/service/ArticleService.java
+++ src/main/java/com/example/service/ArticleService.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -38,10 +39,10 @@ public class ArticleService {
             case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
             case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
             case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
-            case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
-            // TODO : 해시태그에 자동으로 #을 붙여주도록 함.
-            //  #을 사용자가 입력한 경우는 해당 코드로 해결 불가.
-            //  추후 리팩토링
+            case HASHTAG -> articleRepository.findByHashtagNames(
+                    Arrays.stream(searchKeyword.split(" ")).toList(), pageable
+            )
+                    .map(ArticleDto::from);
         };
     }
 
@@ -76,7 +77,6 @@ public class ArticleService {
                 if (dto.content() != null) {
                     article.setContent(dto.content());
                 }
-                article.setHashtag(dto.hashtag());
             }
         } catch (EntityNotFoundException e) {
             log.warn("게시글 업데이트 실패. 게시글을 수정하는 데에 필요한 정보를 찾을 수 없습니다 - {}", e.getLocalizedMessage());
@@ -96,7 +96,7 @@ public class ArticleService {
         if (hashtag == null || hashtag.isBlank()) {
             return Page.empty(pageable);
         }
-        return articleRepository.findByHashtag(hashtag, pageable).map(ArticleDto::from);
+        return articleRepository.findByHashtagNames(null, pageable).map(ArticleDto::from);
     }
 
     public List<String> getHashtags() {
