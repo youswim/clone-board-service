762ad93db005a6ac90554366bcd8724c1f1c053f
youswim <youswim96@gmail.com>
2023-03-31 15:14:44 +0900

    [Feat]: 클라이언트에 필요한 정보 model에 추가
    이 정보는 뷰에서 해시태그에 링크를 달 때
    게시글의 검색 필터 정보로 쓰기 위해 필요하다.

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -40,6 +40,8 @@ public class ArticleController {
         map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumbers);
         map.addAttribute("searchTypes", SearchType.values());
+        map.addAttribute("searchTypeHashtag", SearchType.HASHTAG);
+
         return "articles/index";
     }
 
@@ -49,6 +51,7 @@ public class ArticleController {
         map.addAttribute("article", article); // TODO: 실제 데이터를 찾아서 넣어줘야 하는 부분
         map.addAttribute("articleComments", article.articleCommentsResponse());
         map.addAttribute("totalCount", articleService.getArticleCount());
+        map.addAttribute("searchTypeHashtag", SearchType.HASHTAG);
 
         return "articles/detail";
     }
