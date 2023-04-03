d6425dd8d06cab6e74caf84f45fb77c32281d6f9
youswim <youswim96@gmail.com>
2023-04-03 21:06:15 +0900

    [Feat]: 댓글 도메인에 대댓글을 위한 코드 추가

--- src/main/java/com/example/domain/ArticleComment.java
+++ src/main/java/com/example/domain/ArticleComment.java
@@ -1,10 +1,13 @@
 package com.example.domain;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
-
+import java.util.Set;
 
 @Getter
 @ToString
@@ -25,9 +28,18 @@ public class ArticleComment extends AuditingFields {
     private Article article;
 
     @Setter
-    @ManyToOne(optional = false)
     @JoinColumn(name = "userId")
+    @ManyToOne(optional = false)
     private UserAccount userAccount;
+
+    @Setter
+    @Column(updatable = false)
+    private Long parentCommentId;
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
 
     @Setter
     @Column(nullable = false, length = 500)
@@ -35,15 +47,22 @@ public class ArticleComment extends AuditingFields {
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+        return new ArticleComment(article, userAccount, null, content);
     }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
