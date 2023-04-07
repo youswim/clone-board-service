aec364ac290ea0c6114246a0a5117c09517404d8
youswim <youswim96@gmail.com>
2023-04-07 23:59:00 +0900

    [Feat]: UserAccountService 추가
    OAuth를 사용하면서,
    회원을 DB에 저장하는 로직 필요.
    
    `UserAccountService`를 만드는 김에
    `UserDetailService`에서 `UserAccountRepository`를 사용하는 로직을 `UserAccountService`를 사용하도록 만들기 위해 `searchUser` 메서드 추가

---
+++ src/main/java/com/example/service/UserAccountService.java
@@ -0,0 +1,29 @@
+package com.example.service;
+
+
+import com.example.domain.UserAccount;
+import com.example.dto.UserAccountDto;
+import com.example.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class UserAccountService {
+    private final UserAccountRepository userAccountRepository;
+
+    public Optional<UserAccountDto> searchUser(String username) {
+        return userAccountRepository.findById(username)
+                .map(UserAccountDto::from);
+    }
+
+    public UserAccountDto saveUser(String username, String password, String email, String nickname, String memo) {
+        return UserAccountDto.from(
+                userAccountRepository.save(UserAccount.of(username, password, email, nickname, memo))
+        );
+    }
+}
